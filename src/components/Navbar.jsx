ed6275039c6f199e8688c35b2672feb29d51a080
impcakash <impcakash@gmail.com>
2025-05-06 17:42:24 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,175 @@
+"use client";
+
+import { useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useColorMode } from "./ThemeRegistry";
+import { useTheme } from "@mui/material/styles";
+
+const pages = [
+  { name: "HOME", path: "/" },
+  { name: "EXCHANGE RATES (LIVE)", path: "/exchange-rates" },
+  { name: "ABOUT", path: "/about" },
+  { name: "ERROR PAGE", path: "/error-demo" },
+];
+
+export default function Navbar() {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const colorMode = useColorMode();
+  const theme = useTheme();
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Desktop Logo */}
+          <Typography
+            variant="h6"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontWeight: 700,
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Loan Calculator
+          </Typography>
+
+          {/* Mobile Menu Button */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.path}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={page.path}
+                  selected={pathname === page.path}
+                >
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Mobile Logo */}
+          <Typography
+            variant="h5"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontWeight: 700,
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Loan Calculator
+          </Typography>
+
+          {/* Desktop Menu and Theme Toggle */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page.path}
+                component={Link}
+                to={page.path}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: 1,
+                  color: "white",
+                  backgroundColor:
+                    pathname === page.path
+                      ? "rgba(255, 255, 255, 0.2)"
+                      : "transparent",
+                  "&:hover": {
+                    backgroundColor: "rgba(255, 255, 255, 0.3)",
+                  },
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+
+            {/* Theme Toggle Button */}
+            <IconButton
+              sx={{ ml: 2 }}
+              onClick={colorMode.toggleColorMode}
+              color="inherit"
+              aria-label="toggle dark mode"
+            >
+              {theme.palette.mode === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <Brightness4Icon />
+              )}
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
