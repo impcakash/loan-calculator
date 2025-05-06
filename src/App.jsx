8df3c2679e62a8f85ed23de4605ce815cb489798
impcakash <impcakash@gmail.com>
2025-05-06 17:15:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeRegistry";
+import { CurrencyProvider } from "./contexts/CurrencyContext";
+import HomePage from "./pages/HomePage";
+import ExchangeRatesPage from "./pages/ExchangeRatesPage";
+import AboutPage from "./pages/AboutPage";
+import ErrorDemoPage from "./pages/ErrorDemoPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <ThemeProvider>
+      <CurrencyProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/exchange-rates" element={<ExchangeRatesPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/error-demo" element={<ErrorDemoPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Router>
+      </CurrencyProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
